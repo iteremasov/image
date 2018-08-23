c102b725fe349962a6cab2e826aff1909553e6ee
iteremasov <teremasovjr@gmail.com>
2018-08-23 22:58:03 +0600

    create url /img/.../...

---
+++ site.py
@@ -0,0 +1,25 @@
+from bottle import route, run, template, SimpleTemplate, static_file, response
+from PIL import Image
+import io
+
+
+@route('/img/<width>/<height>')
+def img_gen(width, height):
+    height = int(height)
+    width = int(width)
+    img = Image.new("RGB", (height, width), (0, 0, 0))
+    ff = io.BytesIO()
+    img.save(ff, 'jpeg')
+    bytes_img = ff.getvalue()
+    response.content_type = 'image/jpeg'
+    response.content_length = len(bytes_img)
+    return bytes_img
+
+
+
+@route('/statistic')
+def get_statistic(request):
+    
+    return
+
+run(host='localhost', port=8000)
